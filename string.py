9fdfbaf28cb769641181e3d881f3ef449e289204
z5214546@student.unsw.edu.au <z5214546@student.unsw.edu.au>
2018-07-25 17:45:40 +1000

    Added a string.py

---
+++ string.py
@@ -0,0 +1,10 @@
+strings = ['This', 'list', 'is', 'now', 'all', 'together']
+sentence = ''
+flag = False
+for x in strings:
+    if flag is True:
+        sentence = sentence + ' '
+    sentence += x
+    flag = True
+print (sentence)
+    
